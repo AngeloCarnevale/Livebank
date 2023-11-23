8e991ceaf34af1b270c90ef8631fa28425aa0ba3
AngeloCarnevale <angelo.carnevale@hotmail.com>
2023-11-23 16:59:50 -0300

    adding image field in UserModel

--- apps/poc-api/authentication/models.py
+++ apps/poc-api/authentication/models.py
@@ -1,13 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from uuid import uuid4
 
+
+def upload_image_profile(instance, filename):
+    return f"{instance.id}-{filename}"
 
 class UserModel(AbstractUser):
-    
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid4)
     name = models.CharField(max_length=150)
     email = models.EmailField(max_length=150, unique=True)
     password = models.CharField(max_length=100)
     username = None
+    image = models.ImageField(upload_to=upload_image_profile, blank=True, null=True)
     
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
--- apps/poc-api/core/urls.py
+++ apps/poc-api/core/urls.py
@@ -4,6 +4,8 @@ from authentication.api.viewsets import UserViewSet
 from bank_operations.api.viewsets import AccountViewSet,CardViewSet,  AddressViewSet, ContactsViewSet, TransactionViewSet, DepositViewSet
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 router = routers.DefaultRouter()
@@ -28,4 +30,4 @@ urlpatterns = [
         ),
         name="swagger-ui",
     ),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
