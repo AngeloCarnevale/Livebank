df89b3e0beb42be86271e4c8c06f7c64fe8fe729
AngeloCarnevale <angelo.carnevale@hotmail.com>
2023-11-27 10:17:00 -0300

    block account after 3 requests

--- apps/poc-api/core/settings.py
+++ apps/poc-api/core/settings.py
@@ -144,4 +144,12 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '3/minute',
+        'user': '5000/minute'
+    },
 }
